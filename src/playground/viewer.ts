372f49dd67bac2c62a4bf4c89a943f917c8332f2
Kimio Kuramitsu <kkuramitsu@gmail.com>
2020-02-05 03:28:11 +0900

    viewer

---
+++ src/playground/viewer.ts
@@ -0,0 +1,29 @@
+import 'katex/dist/katex.min.css'   // *** 必須です！
+import MarkdownIt from 'markdown-it';
+//import mk from 'markdown-it-katex';
+const mk = require('markdown-it-katex');
+
+const md = new MarkdownIt("commonmark", {
+  //injected: true, // $mdを利用してmarkdownをhtmlにレンダリングする
+  breaks: true, // 改行コードを<br>に変換する
+  html: true, // HTML タグを有効にする
+  linkify: true, // URLに似たテキストをリンクに自動変換する
+  //typography: true,  // 言語に依存しないきれいな 置換 + 引用符 を有効にします。
+});
+md.use(mk);
+
+export class PuppyViewer {
+  element: HTMLElement;
+  public constructor(element: HTMLElement, options = {}) {
+    this.element = element;
+  }
+
+  public setHTMLDocument(doc: string) {
+    this.element.innerHTML = doc;
+  }
+
+  public setMarkdownDocument(doc: string) {
+    this.element.innerHTML = md.render(doc);
+  }
+
+}
