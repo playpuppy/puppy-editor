c7b83ab9debaf3331f79cbff8e08511e6d5dc765
tdtk <tadataku.lab@gmail.com>
2020-07-07 23:30:25 +0900

    Updates

---
+++ demo-viewer.ts
@@ -0,0 +1,11 @@
+//import { PuppyVM, PuppyCode, PuppyWorld } from './puppyvm/vm';
+import { PuppyViewer } from '../src/playground/viewer';
+
+const viewer = new PuppyViewer(document.body);
+viewer.setMarkdownDocument(`
+# 正規分布です。 
+$f(x) = \\frac{1}{\\sqrt {2\\pi \\sigma^2}} \\exp\\Biggl(-\\frac{(x-\\mu)^2}{2\\sigma^2}\\Biggr) \\qquad (-\\infty<x<\\infty)$
+
+`);
+//editor.addLineHighLight(2, 2);
+
